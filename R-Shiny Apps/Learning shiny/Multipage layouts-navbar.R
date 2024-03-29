f114e9f56027b29d2081b4455824288c7d019488
Nsubuga Emmanuel Reagan <nsubugaemmanuel1@gmail.com>
2024-03-29 16:38:16 +0300

    Add files via upload

---
+++ R-Shiny Apps/Learning shiny/Multipage layouts-navbar.R
@@ -0,0 +1,15 @@
+library(shiny)
+
+ui <- navbarPage( 
+  "Page title", 
+  tabPanel("panel 1", "one"), 
+  tabPanel("panel 2", "two"), 
+  tabPanel("panel 3", "three"), 
+  navbarMenu("subpanels", 
+             tabPanel("panel 4a", "four-a"), 
+             tabPanel("panel 4b", "four-b"), 
+             tabPanel("panel 4c", "four-c") 
+  )
+)
+
+shinyApp(ui, server)
